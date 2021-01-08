c953fdfc8df93dd4655e81206d8a0b396a677826
tanakarn98 <56716847+tanakarn98@users.noreply.github.com>
2021-01-08 23:49:30 +0900

    done

---
+++ app.js
@@ -0,0 +1,93 @@
+window.onload = init();
+function init() {
+  initAnimation();
+
+  function initAnimation() {
+    var canvas = document.getElementById("canvas");
+    var ctx = canvas.getContext("2d");
+
+    var imagePath = "./sample.jpg";
+    var image = new Image();
+    image.src = imagePath;
+
+    //set canvas width and height
+    canvas.width = Number(window.innerWidth);
+    canvas.height = Number(canvas.width / 2);
+    image.onload = function () {
+      initDraw();
+      loop();
+    };
+
+    var canvasEndX = canvas.width;
+    var canvasEndY = canvas.height;
+    var waveStartPoint = canvasEndY - 150;
+
+    var amplitude = 30;
+    var period = 600;
+    var degree = 0;
+
+    function initDraw() {
+      imageSet(image, canvasEndX, canvasEndY);
+      waveDrawing(
+        waveStartPoint,
+        canvasEndX,
+        canvasEndY,
+        degree,
+        amplitude,
+        period
+      );
+    }
+
+    function loop() {
+      setInterval(function () {
+        imageSet(image, canvasEndX, canvasEndY);
+        waveDrawing(
+          waveStartPoint,
+          canvasEndX,
+          canvasEndY,
+          degree,
+          amplitude,
+          period
+        );
+        degree += 12;
+      }, 30);
+    }
+
+    function imageSet(imageObj, canvasEndX, canvasEndY) {
+      var imgWidth = imageObj.width;
+      var imgHeight = imageObj.height;
+
+      ctx.globalCompositeOperation = "destination-over";
+      ctx.drawImage(
+        image,
+        0,
+        0,
+        imgWidth,
+        imgHeight,
+        0,
+        0,
+        canvasEndX,
+        canvasEndY
+      );
+    }
+
+    function waveDrawing(waveStartPoint, canvasEndX, canvasEndY, deg, am, tp) {
+      var waveStartY = waveStartPoint;
+      ctx.globalCompositeOperation = "destination-out";
+      ctx.beginPath();
+      ctx.moveTo(0, waveStartY);
+
+      for (var x = 0; x <= canvasEndX; x += 1) {
+        var y = -am * Math.sin((Math.PI / tp) * (deg + x));
+        ctx.lineTo(x, y + waveStartY);
+      }
+
+      ctx.lineTo(canvasEndX, canvasEndY);
+      ctx.lineTo(0, canvasEndY);
+      ctx.closePath();
+
+      ctx.fillStyle = "rgba(255,255,255,1)"; //opacity 1
+      ctx.fill();
+    }
+  }
+}
